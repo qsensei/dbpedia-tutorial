d9c62893ca73c1a3e170f81830f943080e5e0a69
Geoffrey Chan <gc@qsensei.com>
2016-01-27 19:55:37 -0800

    chore: use built in transformer for to_year transformer

--- schema/customtransformers.py
+++
@@ -1,9 +0,0 @@
-from datetime import date
-from datetime import datetime
-
-
-def to_year(values):
-    for value in values:
-        if not isinstance(value, date) or isinstance(value, datetime):
-            continue
-        yield str(value.year)
